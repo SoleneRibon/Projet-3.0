ae8bc97c281b2dd2486a054c617078e7f7f89c16
Solène Ribon <ribonsolene@gmail.com>
2023-10-03 15:23:07 +0200

    commit 03/10/2023

--- FrontEnd/script/index.js
+++ FrontEnd/script/index.js
@@ -4,8 +4,12 @@ const modaleBack = document.getElementById('modale');
 const divModale = document.querySelector('.modaleBox');
 const figureModale = document.querySelector('.figure-img')
 const crossModale = document.querySelector('.cross-modale');
+const crossModaleImg = document.querySelector('.cross-modaleImg')
 const topBar = document.querySelector('.top-info');
 const editBtn = document.querySelector('.edit-btn');
+const btnAddImg = document.querySelector('.addImg');
+const AddImgPage = document.querySelector('.addImgModale')
+const arrowModal = document.querySelector('.arrow-modale');
 
 
 
@@ -21,32 +25,33 @@ const filtersDiv = document.querySelector("#filters")
 async function init() {
 	const worksPromise = getDatabaseData("works")
 	const catsPromise = getDatabaseData("categories")
-	const works = await Promise.all([worksPromise])
-	const cats = await Promise.all([catsPromise])
+	const works = await worksPromise
+	const cats = await catsPromise
 
-	for (const work of works[0]) {
+	for (const work of works) {
 		allWorks.add(work)
 	}
 
-	for (const cat of cats[0]) {
+	for (const cat of cats) {
 		allCats.add(cat)
 	}
 
 	displayWorks()
 	displayFilter()
+	closeModal()
 }
 init()
 
 async function getDatabaseData(type) {
-    try {
-        const responseData = await fetch(apiUrl + type);
-        const allData = await responseData.json();
-        return allData;
-    } catch (error) {
-        console.error(error);
-    }
+	try {
+		const responseData = await fetch(apiUrl + type);
+		const allData = await responseData.json();
+		return allData;
+	} catch (error) {
+		console.error(error);
+	}
 };
-
+//génération des works
 function displayWorks(filter = "0") {
 	gallery.innerHTML = ""
 	let selectedWorks = allWorks
@@ -64,17 +69,7 @@ function displayWorks(filter = "0") {
 	gallery.appendChild(fragment)
 }
 
-function displayWorksIntoModal() {
-	figureModale.innerHTML = ''
-	const fragment = document.createDocumentFragment()
-	for (const work of allWorks) {
-		const figure = document.createElement("figure");
-		figure.innerHTML = `<img src="${work.imageUrl}" alt="${work.title}"/>`
-		fragment.appendChild(figure)
-	}
-	figureModale.appendChild(fragment)
-}
-
+//activations des filtres avec les boutons
 function addFiltersHandler() {
 	const filtersButton = document.querySelectorAll('.filters-button');
 	for (const button of filtersButton) {
@@ -97,7 +92,7 @@ function displayFilter(filter = '0') {
 		button.classList.add('active')
 	}
 	fragment.appendChild(button)
-	
+
 	// Les autres boutons
 	for (const cat of allCats) {
 		const catButton = document.createElement('button');
@@ -110,15 +105,13 @@ function displayFilter(filter = '0') {
 		fragment.appendChild(catButton)
 
 	}
-	
+
 	filtersDiv.appendChild(fragment)
 	addFiltersHandler()
 
 
 
 }
-
-
 
 //*****************Si connecté ********************/
 
@@ -153,47 +146,240 @@ if (isLogged) {
 
 
 }
-/*********************************
- *********************************
- *********************************
-	Modale fct
+/**************************************
+ **************************************
+ **************************************
+			Modale fct
 	
-**********************************
-**********************************
-**********************************/
+***************************************
+***************************************
+**************************************/
+//fonction pour la modale
+function displayWorksIntoModal() {
 
+	figureModale.innerHTML = ''
+	const fragment = document.createDocumentFragment()
+	for (const work of allWorks) {
+		const figure = document.createElement("figure");
+		figure.classList.add('figureWorksModal');
+		figure.setAttribute('data-id', work.id)
+		figure.innerHTML = `<img src="${work.imageUrl}" alt="${work.title}"/> 
+		<div class="trash">
+		<i class="btnTrash fa-solid fa-trash-can"></i>
+		</div>`
 
+		fragment.appendChild(figure)
+
+	}
+	figureModale.appendChild(fragment)
+
+	deleteWork();
+}
 //montrer la modale
 function displayModale() {
 	modaleBack.style.display = null
+	divModale.style.display = null
 	crossModale.addEventListener('click', closeModal);
 	displayWorksIntoModal();
 }
 //fermer la modale
 function closeModal() {
-	
-	modaleBack.style.display = "none"
-	modaleBack.setAttribute('aria-hidden', 'true');	
+	divModale.style.display = "none";
+	AddImgPage.style.display = "none";
+	modaleBack.style.display = "none";
+	modaleBack.setAttribute('aria-hidden', 'true');
+	formAddWork.reset()
+	document.querySelector('#previewImage').src = "#"
+	document.querySelector('.addImgContainer').classList.remove('hide')
+	document.querySelector('.btnSubmit').style.backgroundColor = '#a7a7a7';
 }
 
-modaleBack.addEventListener('click', function(event){
-	if(event.target === modaleBack){
+modaleBack.addEventListener('click', function (event) {
+	if (event.target === modaleBack) {
 		closeModal();
 	}
 });
 
-//récupération du bouton et ajout du listener pour montrer la modale
+// Récupération du bouton et ajout du listener pour montrer la modale
 
 editBtn.addEventListener('click', () => {
 	displayModale();
 
 });
 
+// Function pour récupérer le projet en fonction de son ID
+
+function findWorkById(id) {
+	for (const work of allWorks) {
+		if (work.id === id) {
+			return work;
+		}
+	}
+}
+
+// Suppression des travaux
+
+async function deleteProject(worksId) {
+	const apiUrlWorks = "http://localhost:5678/api/works";
+	const deleteUrl = `${apiUrlWorks}/${worksId}`;
+	console.log(deleteUrl)
+
+	if (confirm) {
+		const response = await fetch(deleteUrl, {
+			method: 'DELETE',
+			headers: {
+				Authorization: `Bearer ${userToken}`,
+			}
+		});
+
+		if (!response.ok) {
+			throw new Error('Une erreur a eu lieu pendant la suppression du projet');
+		} else {
+			const workstoRemove = document.querySelector(`.figureWorksModal[data-id="${worksId}"]`);
+			if (workstoRemove) {
+				workstoRemove.remove();
+				alert('Suppression du projet avec succès');
 
 
+				// Actualisation de la gallerie des projets
+				const workToRemove = findWorkById(parseInt(worksId));
+
+				if (workToRemove) {
+					allWorks.delete(workToRemove);
+					displayWorks();
+				}
+
+			} else {
+				console.log('Élément introuvable')
+			}
+		}
+	}
+}
+
+// Supression du Work sélectionné
+function deleteWork() {
+	const btnDelete = document.querySelectorAll(".btnTrash");
+	if (btnDelete) {
+		for (const button of btnDelete) {
+			button.addEventListener("click", () => {
+				const worksId = button.closest('.figureWorksModal').dataset.id;
+				deleteProject(worksId);
 
 
+			})
+		}
+	}
 
+}
 
+//Lancement seconde page Modale (Ajout Photo)
+btnAddImg.addEventListener('click', () => {
+	AddImgPage.style.display = 'flex';
+	// Cacher la première page
+	const firstPageModal = document.querySelector('.modaleBox');
+	firstPageModal.style.display = 'none';
 
+	// Toggle firstPageModal & addImgModal
+	arrowModal.addEventListener('click', (event) => {
+		if (event.target === arrowModal) {
+			AddImgPage.style.display = 'none';
+			firstPageModal.style.display = 'flex';
+		}
+	})
+	// Fermeture modale et remise à zéro
+	crossModaleImg.addEventListener('click', closeModal);
+
+	initCategoryField()
+})
+
+function initCategoryField() {
+	const fragment = document.createDocumentFragment()
+	for (const cat of allCats) {
+		const option = document.createElement('option')
+		option.value = cat.id
+		option.innerHTML = cat.name
+		fragment.appendChild(option)
+	}
+	const categoryField = document.querySelector('#category')
+	categoryField.innerHTML = ''
+	categoryField.appendChild(fragment)
+}
+
+async function sendAddWork() {
+	let formData = new FormData(formAddWork)
+	
+	const response = await fetch(apiUrl + "works", {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${userToken}`,
+		},
+		body: formData
+	});
+
+	console.log(response)
+
+	if (response.ok) {
+		
+		const work = response.json()
+		allWorks.add(await work)
+		displayWorks()
+		displayWorksIntoModal()
+		formAddWork.reset()
+		document.querySelector('#previewImage').src = "#"
+		document.querySelector('.addImgContainer').classList.remove('hide');
+		document.querySelector('.btnSubmit').style.backgroundColor = '#a7a7a7';
+		alert("Le formulaire a été envoyé avec succès.")
+		closeModal()
+	} else if (response.status === 401) {
+		alert("Impossible de soumettre le formulaire car vous n'êtes pas authentifié.")
+	} else if (response.status === 400) {
+		alert("Le formulaire soumis est invalide")
+	 } else if (response.status === 500) {
+		alert("Une erreur est survenue côté API")
+	}
+}
+
+const formAddWork = document.querySelector('#addWorkForm')
+const btnAddWork = document.querySelector('.btnSubmit')
+
+btnAddWork.addEventListener('click', function () {
+	console.log(addWorkFormIsValid())
+	if (addWorkFormIsValid()) {
+		sendAddWork()
+	} else {
+		alert('Une donnée du formulaire est vide.');
+	}
+})
+
+function addWorkFormIsValid() {
+	let titleValue = formAddWork.querySelector('input[name="title"]').value;
+	let imageValueIsNotNull = inputFile.files && inputFile.files[0];
+	return titleValue !== null && titleValue !== undefined && titleValue.trim() !== '' && imageValueIsNotNull;
+}
+
+const inputFile = document.querySelector('#image')
+inputFile.onchange = (event) => {
+	let target = event.target
+	let selectedFile = target.files && target.files[0] ? target.files[0] : undefined;
+	if (selectedFile != undefined) {
+		var fileReader = new FileReader();
+        fileReader.onload = function (fileEventReader) {
+			document.querySelector('#previewImage').src = fileEventReader.target.result;
+			document.querySelector('.addImgContainer').classList.add('hide')
+			document.querySelector('.btnSubmit').style.backgroundColor = '#1d6154';
+    	};
+        fileReader.readAsDataURL(selectedFile);
+	} else {
+		document.querySelector('.addImgContainer').classList.remove('hide')
+	}
+}
+
+// Bouton Import Image
+const fakeBtnImg = document.querySelector('.addImgBtn');
+const importBtn = document.querySelector('#image');
+
+fakeBtnImg.addEventListener('click', (event) => {
+	event.preventDefault();
+	importBtn.click();
+});
 
